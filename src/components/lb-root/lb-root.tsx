0f6c4e8b387b8a95a676997f0444a2303af263e2
Steve Ruiz <steveruizok@gmail.com>
2018-07-18 09:42:58 +0100

    tweaks

--- src/components/lb-root/lb-root.tsx
+++ src/components/lb-root/lb-root.tsx
@@ -2,15 +2,20 @@ import { Component } from "@stencil/core";
 import * as looks from "./looks";
 
 @Component({
-  tag: "lb-root",
-  styleUrl: "lb-root.css"
+	tag: "lb-root",
+	styleUrl: "lb-root.css"
 })
 export class LbRoot {
-  render() {
-    return (
-      <main>
-        <lb-book looks={looks} />
-      </main>
-    );
-  }
+	render() {
+		return (
+			<main class="narrow">
+				<h3>st-lookbook</h3>
+				<p>@steveruizok</p>
+				<p>
+					<code>npm i st-lookbook</code>
+				</p>
+				<lb-book looks={looks} />
+			</main>
+		);
+	}
 }
